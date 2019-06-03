7394b648e95b74496585115c317edab9fefb83c5
Mostafa Amini Nasab <mostafa.amini.nasab@gmail.com>
2019-06-04 04:24:10 +0430

    Scaffold the project

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+var babel = require("rollup-plugin-babel");
+var resolve = require("rollup-plugin-node-resolve");
+var terser = require("rollup-plugin-terser").terser;
+var filesize = require("rollup-plugin-filesize");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const name = "CommonObjectOperations";
+
+const destBase = "dist/index";
+const destExtension = `${isProduction ? ".min" : ""}.js`;
+
+module.exports = {
+  input: "src/index.js",
+  output: [
+    {
+      file: `${destBase}${destExtension}`,
+      format: "cjs"
+    },
+    {
+      file: `${destBase}.esm${destExtension}`,
+      format: "esm"
+    },
+    {
+      file: `${destBase}.umd${destExtension}`,
+      format: "umd",
+      name: name
+    },
+    {
+      file: `${destBase}.amd${destExtension}`,
+      format: "amd",
+      name: name
+    },
+    {
+      file: `${destBase}.browser${destExtension}`,
+      format: "iife",
+      name: name
+    }
+  ],
+  plugins: [resolve(), babel(), isProduction && terser(), filesize()].filter(
+    Boolean
+  )
+};
